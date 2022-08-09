8df40c4a65eee286a16a91b4db5cc6ddd014b8c5
simba155korea <simba155korea@gmail.com>
2022-08-10 01:05:16 +0900

    Backend : 미션 기능

--- backend/src/main/java/com/simba/missonGame/controller/MissionController.java
+++ backend/src/main/java/com/simba/missonGame/controller/MissionController.java
@@ -4,12 +4,14 @@ import com.simba.missonGame.dto.mission.GetOneMissionRes;
 import com.simba.missonGame.service.MissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/mission")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MissionController {
 
     @Autowired
--- backend/src/main/java/com/simba/missonGame/db/repository/MissionRepository.java
+++ backend/src/main/java/com/simba/missonGame/db/repository/MissionRepository.java
@@ -3,7 +3,9 @@ package com.simba.missonGame.db.repository;
 import com.simba.missonGame.db.entity.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 
-
+    public Optional<Mission> findByMissionNo(Long missionNo);
 }
